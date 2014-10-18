fe80a17d2b7768909eca0d05b6d10b3ddd0809fe
Zachary Nagler <zachary.nagler@gmail.com>
2014-10-18 19:14:44 -0400

    finished basic functionality

--- public/js/application.js
+++ public/js/application.js
@@ -4,15 +4,24 @@ function playOn(){
   var store_dict = localStorage.getItem('dict');
   var dict = JSON.parse(store_dict);
   $("#inputz").val("")
-  if (entry === "J"){
+  var jumps = parseInt($("#jumps").text())
+  if (entry === "J"  && jumps>0){
     Animate.shift(1)
+    jumps--
+    $("#jumps").text(jumps)
     return
   }
   if (dict[entry]){
     $('#statusz').text("Real word")
-    var next_five = $('#hidestring').text().substring(0,5).toUpperCase()
-    var shift_length = Logic.analyze(entry,next_five)
-    Animate.shift(shift_length);}
+    var nextFive = $('#hidestring').text().substring(0,5).toUpperCase()
+    currentLogic = new logic(entry,nextFive)
+    // var shift_length = Logic.analyze(entry,next_five)
+    Animate.shift(currentLogic.shiftLength())
+    jumps += currentLogic.addedJumps()
+    $("#jumps").text(jumps)
+
+  }
+
   else{
       $('#statusz').text("Fake word")
     }
