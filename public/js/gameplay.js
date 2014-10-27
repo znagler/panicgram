42ee1f5721bdedff5d8cc992e20d06fee0b35f6a
Zachary Nagler <zachary.nagler@gmail.com>
2014-10-26 21:09:26 -0400

    animation

--- public/js/gameplay.js
+++ public/js/gameplay.js
@@ -23,12 +23,14 @@ function playOn(callback){
 
     var nextFive = $("#hidestring").data('hs').substring(0,5).toUpperCase()
     currentLogic = new logic(entry,nextFive)
+    if (currentLogic.shiftLength() >= 1){
     addScore(currentLogic.shiftLength())
     Animate.shift(currentLogic.shiftLength())
     jumps += currentLogic.addedJumps()
     // $("#jumps").text(jumps)
     $("#jumps").data("j",jumps)
     View.updateJ()
+    }
 
   }
 
@@ -53,8 +55,18 @@ function checkForWin(){
 
 function addScore(integer){
   console.log("in add score")
+    // $('#score-container').
+    //red -> 255,0,0
+    //blue 0,0,255
+    //yellow 255,255,0
+    var r=0
+    var g=0
+    var b=0
+    if (integer === 2) b=255;
+    if (integer === 3) r=255;
+    if (integer === 5) g=r=255;
 
-  $('#score-container').append("<div class='score-points'>+"+integer+"</div>")
+    $('#score-container').append("<div class='score-points' style='color: rgba("+r+","+g+","+b+", 0.9)'>+"+integer+"</div>")
   // $('#score').remove()
     new_score = $('#score').data("s") + integer
     $('#score').data("s",new_score)
