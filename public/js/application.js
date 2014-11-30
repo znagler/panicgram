566ddcd30c7b08c6a3360ea1e3e6d91b5a67502a
Zack Nagler <zachary.nagler@gmail.com>
2014-11-30 15:21:44 -0500

    prevent double submission glitch with removal of hidden input on first submit

--- public/js/application.js
+++ public/js/application.js
@@ -16,43 +16,41 @@ Global.startTime = new Date()
   if (checkForWin()) winGame()
   })
 
-
-  $( ".username" ).submit(function( event ) {
-  console.log("test")
-  })
-
-
-
-
 });
 
 
 function winGame(){
+  console.log("wtf")
 
     Global.endTime = new Date()
-    Global.finalTime = Math.round((Global.endTime - Global.startTime) * (1/1000))
-    console.log(Global.finalTime)
+    var rawTime = Math.round((Global.endTime - Global.startTime)*(1/100))
+    Global.finalTime = rawTime + (rawTime+"").slice(-1)
     $('#statusz').text("Final Time: "+Global.finalTime+" seconds")
     $( "#formz" ).off()
-    // $( "#formz" ).submit(function( event ) {
-    //   event.preventDefault()
-    // });    
+    $( "#inputz" ).off()
+    $( "#inputz" ).prop( "disabled", true )
+
   setTimeout(function(){
     $.when($(".wordstring").fadeOut("slow")).done(function() {
-        $( "#inputz" ).off()
         $(".wordstring").hide()
         $(".wordstring").text("WIN")
         $('.wordstring').append("<center><a href='http://panicgram.herokuapp.com/play/10'><h4>PLAY AGAIN</h4></a></center>")
         $(".wordstring").fadeIn( 2000, function() {
+        $( "#inputz" ).prop( "disabled", false )
+        $( "#inputz" ).focus()
           $("#inputz").attr("placeholder", "Enter username")
           $("#inputz").css("color", "red")
           $("#formz").addClass( "username" )
+
           $( "#formz" ).attr("action","/test_route")
           $( "#formz" ).attr("method","post")
           $( "#formz" ).append("<input style='display:none' value="+Global.finalTime+" name='score'>")
-          $( "#formz" ).append("<input type='submit' style='position: absolute; left: -9999px'>")
+          $( "#formz" ).append("<input id='temp-submit' type='submit' style='position: absolute; left: -9999px'>")
           $( "#inputz" ).attr("name","username")
-
+          $( "#formz" ).submit(function( event ) {
+            $('#inputz').attr("readonly", true) 
+            $('#temp-submit').remove()
+          })
         })
       })
   }, 500)
