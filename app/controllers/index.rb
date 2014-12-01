09edbdb186e1fb3f21075d1b693f146af1cf5da1
Zack Nagler <zachary.nagler@gmail.com>
2014-11-30 21:20:06 -0500

    add space

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -10,7 +10,7 @@ end
 
 
 post '/score' do
-	Score.create(score:params[:score],username: params[:username],goal: params[:goal])
+	Score.create(score: params[:score],username: params[:username],goal: params[:goal])
 	redirect '/'
 end
 
