88df3226635f89579bb68367cafede21f9f67aac
Zack Nagler <zachary.nagler@gmail.com>
2014-12-02 16:29:10 -0500

    fix no-refresh-on-back-button issue that occured on Safari

--- public/js/load.js
+++ public/js/load.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
+
+
 	$( "a.btn" ).click(function() {
- 		$("body").empty()
+ 		$(".index").empty()
  		$('body').append("<center id='load'><br><br><br><br><br><br><br><br><br><br><br><br><br><br><h1>loading...</h1></center>")
 	})
 })
