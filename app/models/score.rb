57848528970c776062ff7117405f12a18a699e82
Zack Nagler <zachary.nagler@gmail.com>
2014-12-10 22:12:03 -0500

    create simple validations

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,2 +1,6 @@
 class Score < ActiveRecord::Base
+	validates :score, presence: true
+	validates :username, presence: true
+	validates :goal, presence: true
+	validates :score, numericality: { greater_than: 50 }
 end
